8705e62f4dc050c9a76cbc19ab5788d1dc1cd83c
Jose Donizetti <jdbjunior@gmail.com>
2011-09-06 00:51:05 -0300

    rename

---
+++ src/main/java/br/com/caelum/revolution/tools/cochanges/CoChangeCount.java
@@ -0,0 +1,38 @@
+package br.com.caelum.revolution.tools.cochanges;
+
+public class CoChangeCount {
+
+	private Integer id;
+	private Integer count;
+	private String destiny;
+	private String origin;
+
+	public Integer getId() {
+		return id;
+	}
+
+	public Integer getCount() {
+		return count;
+	}
+
+	public void setCount(Integer count) {
+		this.count = count;
+	}
+
+	public String getDestiny() {
+		return destiny;
+	}
+
+	public void setDestiny(String destiny) {
+		this.destiny = destiny;
+	}
+
+	public String getOrigin() {
+		return origin;
+	}
+
+	public void setOrigin(String origin) {
+		this.origin = origin;
+	}
+
+}
