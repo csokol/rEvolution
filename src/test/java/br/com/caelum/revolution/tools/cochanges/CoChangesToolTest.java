4cb546d4d9cf0ccc877b4dc265bbda73f2d6baf5
Jose Donizetti <jdbjunior@gmail.com>
2011-09-06 00:51:32 -0300

    new test

--- src/test/java/br/com/caelum/revolution/tools/cochanges/CoChangesToolTest.java
+++ src/test/java/br/com/caelum/revolution/tools/cochanges/CoChangesToolTest.java
@@ -2,7 +2,10 @@ package br.com.caelum.revolution.tools.cochanges;
 
 import static org.junit.Assert.assertEquals;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
+
+import java.util.List;
 
 import org.hibernate.Session;
 import org.junit.Before;
@@ -28,28 +31,67 @@ public class CoChangesToolTest {
 	}
 	
 	@Test
-	public void shouldCountOneForTwoFilesCommitted() throws ToolException {
+	public void shouldCountOneForOneCommitWithTwoFiles() throws ToolException {
 		//Given
 		Commit commit = new Commit();
 		
-		Artifact fileA = new Artifact("fileA.java", ArtifactKind.CODE);
+		Artifact fileA = new Artifact("A", ArtifactKind.CODE);
 		commit.addArtifact(fileA);
 		
-		Artifact fileB = new Artifact("fileB.java", ArtifactKind.CODE);
+		Artifact fileB = new Artifact("B", ArtifactKind.CODE);
 		commit.addArtifact(fileB);
 		
 		//When
 		tool.calculate(commit, new BuildResult("anything"));
 		
 		//Then
-		ArgumentCaptor<CoChangesCount> argument = ArgumentCaptor.forClass(CoChangesCount.class);
+		ArgumentCaptor<CoChangeCount> argument = ArgumentCaptor.forClass(CoChangeCount.class);
 		verify(session).save(argument.capture());
 		
-		CoChangesCount coChangesCount = argument.getValue();
+		CoChangeCount coChangesCount = argument.getValue();
 		
 		assertEquals(Integer.valueOf(1),coChangesCount.getCount());
-		assertEquals("fileA.java",coChangesCount.getOrigin());
-		assertEquals("fileB.java",coChangesCount.getDestiny());
-		
+		assertEquals("A",coChangesCount.getOrigin());
+		assertEquals("B",coChangesCount.getDestiny());
 	}
+	
+	@Test
+	public void shouldCountOneForEachFileAtTheCommit() throws ToolException {
+		//Given
+		Commit commit = new Commit();
+		
+		Artifact fileA = new Artifact("A", ArtifactKind.CODE);
+		commit.addArtifact(fileA);
+		
+		Artifact fileB = new Artifact("B", ArtifactKind.CODE);
+		commit.addArtifact(fileB);
+		
+		Artifact fileC = new Artifact("C", ArtifactKind.CODE);
+		commit.addArtifact(fileC);
+		
+		//When
+		tool.calculate(commit, new BuildResult("anything"));
+		
+		//Then
+		ArgumentCaptor<CoChangeCount> argument = ArgumentCaptor.forClass(CoChangeCount.class);
+		verify(session, times(3)).save(argument.capture());
+		
+		List<CoChangeCount> coChangesCounts = argument.getAllValues();
+		
+		assertEquals(3,coChangesCounts.size());
+		
+		assertEquals(Integer.valueOf(1),coChangesCounts.get(0).getCount());
+		assertEquals("A",coChangesCounts.get(0).getOrigin());
+		assertEquals("B",coChangesCounts.get(0).getDestiny());
+		
+		assertEquals(Integer.valueOf(1),coChangesCounts.get(1).getCount());
+		assertEquals("A",coChangesCounts.get(1).getOrigin());
+		assertEquals("C",coChangesCounts.get(1).getDestiny());
+		
+		assertEquals(Integer.valueOf(1),coChangesCounts.get(2).getCount());
+		assertEquals("B",coChangesCounts.get(2).getOrigin());
+		assertEquals("C",coChangesCounts.get(2).getDestiny());
+	}
+	
+	
 }
