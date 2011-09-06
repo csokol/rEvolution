a07ea1f580d745ca139c989d00cbc323ba2aef17
Jose Donizetti <jdbjunior@gmail.com>
2011-09-06 00:50:44 -0300

    renaming cochangecount

--- src/main/java/br/com/caelum/revolution/tools/cochanges/CoChangesTool.java
+++ src/main/java/br/com/caelum/revolution/tools/cochanges/CoChangesTool.java
@@ -1,8 +1,12 @@
 package br.com.caelum.revolution.tools.cochanges;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.hibernate.Session;
 
 import br.com.caelum.revolution.builds.BuildResult;
+import br.com.caelum.revolution.domain.Artifact;
 import br.com.caelum.revolution.domain.Commit;
 import br.com.caelum.revolution.persistence.ToolThatPersists;
 import br.com.caelum.revolution.tools.Tool;
@@ -13,13 +17,36 @@ public class CoChangesTool implements Tool, ToolThatPersists {
 	private Session session;
 
 	public void calculate(Commit commit, BuildResult current) throws ToolException {
-		CoChangesCount coChangesCount = new CoChangesCount();
 		
-		coChangesCount.setOrigin(commit.getArtifacts().get(0).getName());
-		coChangesCount.setDestiny(commit.getArtifacts().get(1).getName());
-		coChangesCount.setCount(1);
+		List<CoChangeCount> coChangesCount = new ArrayList<CoChangeCount>();
+
+		List<Artifact> artifacts = commit.getArtifacts();
 		
-		session.save(coChangesCount);
+		generateCoChanges(artifacts.get(0), artifacts, coChangesCount,1);
+
+		for (CoChangeCount coChangeCount : coChangesCount) {
+			session.save(coChangeCount);
+		}
+	}
+
+	private void generateCoChanges(Artifact origin,List<Artifact> destinyArtifats, List<CoChangeCount> coChangesCount, int begin) {
+		
+		for(int i = begin; i < destinyArtifats.size(); i++) {
+		
+			Artifact destinyArtifact = destinyArtifats.get(i); 
+			CoChangeCount coChangeCount = new CoChangeCount();
+			coChangeCount.setCount(1);
+			coChangeCount.setOrigin(origin.getName());
+			coChangeCount.setDestiny(destinyArtifact.getName());
+			
+			coChangesCount.add(coChangeCount);
+		}
+		
+		if(destinyArtifats.size() == begin) {
+			return;
+		}
+		
+		generateCoChanges(destinyArtifats.get(begin), destinyArtifats, coChangesCount,++begin);
 	}
 
 	public String getName() {
@@ -27,7 +54,7 @@ public class CoChangesTool implements Tool, ToolThatPersists {
 	}
 
 	public Class<?>[] classesToPersist() {
-		return new Class<?>[] { CoChangesCount.class } ;
+		return new Class<?>[] { CoChangeCount.class } ;
 	}
 
 	public void setSession(Session session) {
