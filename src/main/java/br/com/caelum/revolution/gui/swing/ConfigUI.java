182c8d99031281311cfbad7f17d2a19783b2e910
Mauricio Aniche <mauricioaniche@gmail.com>
2011-09-17 11:37:26 -0300

    extracting class

--- src/main/java/br/com/caelum/revolution/gui/swing/ConfigUI.java
+++ src/main/java/br/com/caelum/revolution/gui/swing/ConfigUI.java
@@ -16,6 +16,7 @@ import javax.swing.JTextField;
 import javax.swing.UIManager;
 import javax.swing.UnsupportedLookAndFeelException;
 
+import br.com.caelum.revolution.config.DatabaseConfig;
 import br.com.caelum.revolution.config.MapConfig;
 import br.com.caelum.revolution.scanner.ClassScan;
 
@@ -28,10 +29,12 @@ public class ConfigUI extends JFrame {
 	private JButton crawl;
 	private JButton visualize;
 	private JTextField schema;
+	private DatabaseConfig databaseConfig;
 
 	public ConfigUI() {
 		super("rEvolution");
 		setLayout(new BorderLayout());
+		databaseConfig = new DatabaseConfig();
 
 		createConfigTextBoxes();
 		createButtons();
@@ -42,32 +45,16 @@ public class ConfigUI extends JFrame {
 		pack();
 	}
 
-	// TODO: extract to class
-	private MapConfig configBasedOnInput() {
-		Map<String, String> cfgs = new HashMap<String, String>();
-
-		cfgs.put("driver_class", "com.mysql.jdbc.Driver");
-		cfgs.put("connection_string", "jdbc:mysql://" + host.getText()
-				+ "/" + schema.getText()
-				+ "?useUnicode=true&characterEncoding=UTF-8");
-		cfgs.put("dialect", "org.hibernate.dialect.MySQLInnoDBDialect");
-		cfgs.put("db_user", user.getText());
-		cfgs.put("db_pwd", password.getText());
-		cfgs.put("create_tables", "false");
-
-		MapConfig config = new MapConfig(cfgs);
-		return config;
-	}
-	
 	private void setActionsToButtons() {
 		visualize.addActionListener(new ActionListener() {
 
 			public void actionPerformed(ActionEvent ae) {
 
-				MapConfig config = configBasedOnInput();
-				
-				new VisualizationsUI(config, new ClassScan())
-						.setVisible(true);
+				MapConfig config = databaseConfig.configBasedOnInput(
+						host.getText(), schema.getText(),
+						user.getText(), password.getText());
+
+				new VisualizationsUI(config, new ClassScan()).setVisible(true);
 				setVisible(false);
 			}
 
